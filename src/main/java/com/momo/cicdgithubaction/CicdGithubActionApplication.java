0bee66dae75ace0ccbbe7268b12655be66113235
bayemodoudev <131832441+bayemodoudev@users.noreply.github.com>
2024-02-17 05:36:16 -1200

    github action

---
+++ src/main/java/com/momo/cicdgithubaction/CicdGithubActionApplication.java
@@ -0,0 +1,13 @@
+package com.momo.cicdgithubaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdGithubActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdGithubActionApplication.class, args);
+	}
+
+}
